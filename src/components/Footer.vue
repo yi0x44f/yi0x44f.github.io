420024cc4f15ec0f9b886f88440b88014a77355c
yihua <pao.wen25@gmail.com>
2024-10-29 20:23:51 +0800

    chore: modify footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,11 @@
 
 <template>
 <div class="row bg-dark align-items-center" style="min-height: 60px;">
-    <div class="col-8 align-items-center text-light text-center">
-        Yi's copyright@2024
+    <div class="col-lg-8 col-12 my-3 align-items-center text-light text-center">
+        Copyright © 2020 Yi-Hua.
     </div>
-    <div class="col-4 align-items-center text-light ">
-        <p>
-            Email:yi0x44f@gmail.com
-        </p>
+    <div class="col-lg-4 col-12 mb-3 align-items-center text-light text-center ">
+        yi0x44f@gmail.com
     </div>
 </div>
 
