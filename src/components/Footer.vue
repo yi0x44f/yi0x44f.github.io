8b8721f6d2839cfdd10a4340c90d8452bd8373d8
yihua <pao.wen25@gmail.com>
2024-11-01 12:02:08 +0800

    Edit: Home content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,9 @@
 <template>
 <div class="row bg-dark align-items-center" style="min-height: 60px;">
     <div class="col-lg-8 col-12 my-3 align-items-center text-light text-center">
-        Copyright © 2020 Yi-Hua.
+        Copyright © 2024 Yi-Hua.
     </div>
-    <div class="col-lg-4 col-12 mb-3 align-items-center text-light text-center ">
+    <div class="col-lg-4 col-12 my-lg-3 mb-3 align-items-center text-light text-center ">
         yi0x44f@gmail.com
     </div>
 </div>
